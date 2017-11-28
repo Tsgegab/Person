62f86611e909ab22b3d4e32a08fbf4d31fbc45a0
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2017-11-28 12:26:49 -0500

    initial

---
+++ Person.java
@@ -0,0 +1,56 @@
+public class Person {
+public static main (String[] args){
+	public Person {
+
+	 String name;
+
+	 int age;
+
+	public Person() {
+
+		name = "Tsegab";
+
+		age = 25;
+
+	}
+
+	public Person(String name, int age) {
+
+		this.name = name;
+
+		this.age = age;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+
+	}
+
+	public int getAge() {
+
+		return age;
+
+	}
+
+	public void setAge(int age) {
+
+		this.age = age;
+
+	}
+
+	public String toString() {
+
+		return this.name + " " + this.age;
+
+	}
+
+}
+
